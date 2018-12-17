15df4d6dde3ac22d89dbd9a9d9f59026b614c7f1
nelo360 <nelo360@gmail.com>
2018-12-17 10:49:15 +0100

    dont build arstickers and pixel live wallpaper

--- device.mk
+++ device.mk
@@ -49,14 +49,6 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # Inherit from sdm845-common
 $(call inherit-product, device/xiaomi/sdm845-common/sdm845.mk)
 
-# Playground for GoogleCamera
-PRODUCT_PACKAGES += \
-    Playground
-
-# GoogleCamera
-PRODUCT_PACKAGES += \
-    GoogleCameraMod
-
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/uinput-fpc.idc:system/usr/idc/uinput-fpc.idc \
@@ -68,5 +60,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
 
 # Wallpapers
-PRODUCT_PACKAGES += \
-    WallpapersBReel2018
+#PRODUCT_PACKAGES += \
+#    WallpapersBReel2018
