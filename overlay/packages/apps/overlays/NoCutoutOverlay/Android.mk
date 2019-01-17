3b692a0172847d6bdcf39616403219acec3beabf
nelo360 <nelo360@gmail.com>
2019-01-17 23:59:39 +0100

    beryllium: CutoutOverlay: CleanUp

--- overlay/packages/apps/overlays/NoCutoutOverlay/Android.mk
+++ overlay/packages/apps/overlays/NoCutoutOverlay/Android.mk
@@ -1,16 +1,14 @@
-LOCAL_PATH := $(call my-dir)
+LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_RRO_THEME := DisplayCutoutNoCutout
-LOCAL_CERTIFICATE := platform
-
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
-
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_MODULE_TAGS := optional
 
 LOCAL_PACKAGE_NAME := NoCutoutOverlay
 LOCAL_SDK_VERSION := current
+
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
 
 LOCAL_OVERRIDES_PACKAGES := DisplayCutoutEmulationCornerOverlay DisplayCutoutEmulationDoubleOverlay DisplayCutoutEmulationNarrowOverlay DisplayCutoutEmulationTallOverlay DisplayCutoutEmulationWideOverlay
 
