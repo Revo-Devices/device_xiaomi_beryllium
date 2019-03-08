f1825d131bb436d890e70ea2027dc1e4b21de1af
nelo360 <nelo360@gmail.com>
2019-03-08 12:15:16 +0100

    revo bringup

--- aosp_beryllium.mk
+++ aosp_beryllium.mk
@@ -12,6 +12,9 @@ TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
+# Inherit from custom vendor
+$(call inherit-product, vendor/ANXCamera/config.mk)
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_beryllium
 PRODUCT_DEVICE := beryllium
@@ -24,3 +27,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="beryllium"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi-rev1
+
+export CUSTOM_BUILD_TYPE=Beta
